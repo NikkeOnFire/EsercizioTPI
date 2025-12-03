48e19dc13e4c79f58b850247ea7e0b6039cc85eb
God's weakest soldier <nicola.simo.nove08@gmail.com>
2025-12-03 13:29:12 +0100

    Aggiunto i due file, completato attività 2

--- EsercizioMeteoriti.py
+++ EsercizioMeteoriti.py
@@ -1,12 +1,41 @@
 import matplotlib.pyplot as plt
-import json
+import json, folium
 
-filename = "meteorites.json"
+m = folium.Map(location=(0, 0), zoom_start=5)
+nome_file = "meteorites.json"
 
-try:
-  with open(filename, 'r') as file:
-    dati_meteoriti = json.load(file)
-  print("Dati caricati con successo!")
-  print(f"Numero di meteoriti: {len(dati_meteoriti)}")
-except FileNotFoundError:
-  print(f"Errore: Il file '{filename}' non è stato trovato.")
+with open(nome_file, "r", encoding="utf8") as file:
+  stringa_json = file.read()
+oggetto = json.loads(stringa_json)
+massa = []
+anni = []
+for elem in oggetto:
+        try:
+              massa.append(elem["mass"])
+              anni.append(elem["year"])
+        except:
+               continue
+#Numero meteoriti caduti
+print("Numero di meteoriti caduti:", len(oggetto))
+
+#Stampo le chiavi del primo elemento
+print(oggetto[0].keys())
+
+#Stampo i primi 5 elementi
+cycle = 5
+i = 0
+while(i < cycle):
+      print(oggetto[i])
+      i = i + 1
+
+      
+plt.figure(figsize=(10, 6))
+plt.hist(massa, bins=10, edgecolor='black', color='gold', log=True)
+plt.xlabel("Massa (g)")
+plt.ylabel("Frequenza")
+plt.title("Distribuzione delle Masse dei Meteoriti")
+plt.grid(axis='y', alpha=0.75)
+plt.show()
+
+
+
